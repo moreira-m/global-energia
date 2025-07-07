f6f30106b3dd6ac2bcc96f0738b224af57318632
Matheus Moreira <69878348+moreira-m@users.noreply.github.com>
2025-07-07 20:34:32 -0300

    Initialize React project with Tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl font-semibold">Global Energia</h1>
+    </header>
+  );
+}
+
+export default Header;
